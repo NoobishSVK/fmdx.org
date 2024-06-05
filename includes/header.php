d1f1b51d9bf7fadc29c30e15d24dda5fe17e4d7f
NoobishSVK <ardoniceksvk@gmail.com>
2024-06-05 18:56:14 +0200

    seo shenanigans

--- includes/header.php
+++ includes/header.php
@@ -3,6 +3,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php echo isset($pageTitle) ? $pageTitle.' - FMDX.org - the Open FM DX Hub!' : 'FMDX.org - the Open FM DX Hub!'; ?></title>
-    <link rel="stylesheet" href="/css/entry.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <meta name="description" content="Welcome to FMDX.org, the hub for our worldwide community of DXers dedicated to open-source projects. Join us to make DXing easy, collaborative, and open for all.">
+    <meta name="keywords" content="FMDX, FM DXing, open-source, DXers, radio, projects, community, ESP32, TEF, software, hardware">
+    <meta name="author" content="FMDX.org Team">
+    <link rel="canonical" href="https://fmdx.org">
+    
+    <meta property="og:title" content=<?php echo isset($pageTitle) ? $pageTitle.' - FMDX.org - the Open FM DX Hub!' : 'FMDX.org - the Open FM DX Hub!'; ?>">
+    <meta property="og:description" content="Join the global DXers community dedicated to open-source projects. Discover hardware and software solutions for DXing.">
+    <meta property="og:image" content="https://fmdx.org/img/og-image.jpg">
+    <meta property="og:url" content="https://fmdx.org">
+    <meta property="og:type" content="website">
+    
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="FMDX.org - The Open FM DX Hub">
+    <meta name="twitter:description" content="Explore the community-driven projects and solutions for FM DXing. Join us today!">
+    <meta name="twitter:image" content="https://fmdx.org/img/og-image.jpg">
+    
+    <link rel="icon" href="https://fmdx.org/favicon.ico" type="image/x-icon">
+    <style>
+    /* Critical CSS here */
+    </style>
+    <link rel="stylesheet" href="/css/entry.css" media="print" onload="this.media='all'">
+    <noscript><link rel="stylesheet" href="/css/entry.css"></noscript>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" defer></script>
 </head>
