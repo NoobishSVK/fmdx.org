749c35a149f0664203061deaf2fd36a62ddfaa8a
Marek Farkaš <ardoniceksvk@gmail.com>
2025-01-06 19:33:38 +0100

    december newsletter

---
+++ articles/newsletter-december-2024.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+    $pageTitle = 'Newsletter - December 2024';
+    include($_SERVER['DOCUMENT_ROOT'].'/includes/header.php');
+    ?>
+    <body>
+        <?php include($_SERVER['DOCUMENT_ROOT'].'/includes/navigation.php');?>
+        <div id="wrapper">
+            <div class="flex-container flex-center">
+                <div class="tx-icon"></div>
+                <h1 class="m-0"><span class="color-main text-bold">FMDX.org</span> </h1>
+            </div>
+            <div class="flex-container">
+                <main>
+                    <article class="panel panel-big text-center">
+                        <div class="panel-title flex-container" style="background-image: url('https://i.imgur.com/aL0cNnA.png');">
+                            <div style="width: 100%;">
+                                <h2>Year 2024 Recap & Giveaway winners!</h2>
+                                <p class="text-small-12 m-0">by Noobish & Aurie<br>
+                                6th January 2025</p>
+                            </div>
+                            <div style="width: 100px;font-size:60px;" class="hide-phone">
+                            🎊
+                            </div>
+                        </div>
+
+                        <div class="article-content">
+                        <p class="text-left">It's here, we made it through 2024. So, what did we manage to do in 2024?</p>
+
+                        <h3 class="color-main">Our achievements</h3>
+                        <p class="text-left">2024 has been a groundbreaking year for FM DXing and the community as a whole. It also marked the debut of one of our most ambitious projects: the webserver.
+                            What began as a simple interface to display the current frequency, RDS info, and signal data has evolved into something extraordinary.
+                            Today, we boast hundreds of webservers across the globe, enhanced by plugins developed and managed by our dedicated community members. This incredible achievement wouldn’t have been possible without your support.</p>
+
+                        <p class="text-left">Another milestone this year was the launch of the official FMDX.org website. Designed to keep you informed about the latest DXing news and projects, it has since merged with fmdx.pl and maps.fmdx.pl to form the ultimate platform for DXing enthusiasts.
+                                This unified platform aims to streamline access to tools, resources, and updates, making it easier than ever to stay informed and up-to-date with all the happenings in the community!
+                        </p>
+
+                        <p class="text-left">The headless TEF receiver, featuring the powerful TEF6687 chip and multiple antenna inputs, has also made waves in 2024.
+                            Many DXers have adopted this advanced hardware designed by Konrad Kosmatka & Sjef Verhoeven, enabling them to catch distant signals with remarkable clarity and reduced interference.
+                            It’s worth noting that the ESP32 TEF firmware has reached version 2, a significant milestone in its development. Sjef Verhoeven, one of our founders and most important contributors, continues to work tirelessly on bug fixes and new features as we speak.
+                        </p>
+
+                        <p class="text-left">In addition to these advancements, 2024 saw two episodes of the VHF DX Podcast, managed primarily by American DX enthusiasts,
+                            which featured community members Noobish and Sjef discussing our projects and ideas for the future. These episodes provided valuable insights and stories from the world of DXing,
+                            helping to strengthen connections within the global DXing community.</p>
+
+
+                        <h3 class="color-main">Financial recap</h3>
+                        <p class="text-left">On the financial front, we raised a total of <strong>€463</strong> this year after transaction fees. While these funds remain unused, we plan to invest them wisely in 2025 to ensure our community keeps evolving.
+                            Transparency remains a core value for us, and we’ll continue to keep you updated on how these funds are utilized!</p>
+
+                        <p class="text-left">As we step into 2025, we’re excited to build on the progress we’ve made and explore new opportunities. Thank you for being an integral part of this journey. Here’s to another year of innovation and collaboration in the world of FM DXing!</p>
+
+                        <h3 class="color-main">Christmas giveaway winners</h3>
+                        <p class="text-left">As many of you already know, we hosted a giveaway of multiple supporter tags for our Discord members, along with 3 winners of the TEFs.<br>
+                            This giveaway consisted of 2 parts, the first one was random and all you needed was a bit of luck. The second one was way more difficult to deal with.<br>
+                            The task was simple, yet very interesting - simply post your favourite DX spot, ideally with a nice description and some pictures. We had the opportunity to choose 2 winners.</p>
+                            
+                        <p class="text-left">>We chose the winners based on (but not limited to) this criteria: post description quality, picture quality, how interesting was the spot itself<br><br>
+                        Choosing only two people was very difficult, so every moderator and founder decided to post their top 3 based on the criteria, and the 2 people with most votes were the chosen ones.
+                        We only voted based on the post itself, so previous activity (positive or negative) wasn't counted in any way. And since there were more people, we have decided to gift some more supporter tags.<p>
+
+                        <p class="text-bold">To claim your prize, message Noobish on Discord until 15th of January 2025 under the Discord account you won with.<p>
+
+                        <h3>Winners!</h3>
+                        <p class="text-left text-bold">Supporter tags (1 month): </p>
+                            <ul class="text-left">
+                                <li>kmmvvvnn</li>
+                                <li>theidenterro</li>
+                                <li>markz_9a</li>
+                                <li>rolandsstrautmalis</li>
+                                <li>thedachicken</li>
+                            </ul>
+                        <p class="text-left">Supporter tag (6 months): <span class="font-weight: 300;">confettijr2009</span></p>
+                        <p class="text-left">TEF winners (based on post): <span class="font-weight: 300;">lucasgallone, claudiuneferu2</span></p>
+                        <p class="text-left">TEF winners (based on the wheel of luck): <span class="font-weight: 300;">mcelliotg</span></p>
+
+                        <p class="text-left">Since so many posts were really good, we have also decided to gift these people the supporter for either being in the top 3 of the giveaway wheel, or based on their post quality!</p>
+                        <p class="text-left text-bold">Supporter tags (1 month) - extra winners: </p>
+                            <ul class="text-left">
+                                <li>noahkug</li>
+                                <li>steven_52230</li>
+                                <li>barteqcz</li>
+                                <li>kb8u</li>
+                                <li>yobborobbo21</li>
+                                <li>armandinodx_26041</li>
+                                <li>markz_9a</li>
+                            </ul>
+                        
+                        <h3>Congratulations to everyone!</h3>
+
+                        <p class="text-left">Thank you for joining the giveaway - it was a blast. We really appreciate the tremendous amount of activity FMDX.org is getting and we will definitely love to see more in 2025!</p>
+
+                        <h3 class="color-main">What's coming in 2025?</h3>
+                        <p class="text-left">It's the beginning of the year, however our community has started working on DAB DX Webserver and more important stuff. It's going to get really exciting!</p>
+                        <br>
+                        <img src="/img/logo.png" alt="OpenRadio Logo">
+                        <p>If you have any feedback (positive or negative), please let us know through our Discord server or via e-mail to founders! And as per usual, see you next month.</p>
+                        <br>
+                        <hr>
+                        <p>This article was made by <a href="/founders.php" class="text-bold color-main">Marek Farkaš (Noobish)</a>.<br>If you'd like to support the creator directly, please use the button below.</p>
+                        <p>Your donations are appreciated! ❤️</p>
+                        <a href="https://buymeacoffee.com/noobish" target="_blank"><img src="https://media.licdn.com/dms/image/v2/D4D12AQF0PZYrwThrIQ/article-inline_image-shrink_1000_1488/article-inline_image-shrink_1000_1488/0/1701704819869?e=2147483647&v=beta&t=6RRIU3fx8mdk_gRJLDFtFfyVo4-ny3VOHgr_B2GJgDc" width="250" alt="Buy me a coffee!"></a>
+                        </div>
+                        </article>
+                </main> 
+                <?php include($_SERVER['DOCUMENT_ROOT'].'/includes/sidepanels.php');?>
+            </div>
+        </div>
+        <?php include($_SERVER['DOCUMENT_ROOT'].'/includes/footer.php');?>
+    </body>
+</html>
